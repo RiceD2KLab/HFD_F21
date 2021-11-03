4327fbacf10974880bd1faf48b4c22dc2105ab73
sachethreddy <sm172@rice.edu>
2021-11-02 22:58:03 -0500

    Separated the Shape File Creation and Violation Data based on ZIP Files

--- Interactive maps for Violation in ZIPCODE's/testGeoPyOnHighRiseZipData.py
+++ Interactive maps for Violation in ZIPCODE's/testGeoPyOnHighRiseZipData.py
@@ -3,42 +3,41 @@ from numpy import vectorize
 import pandas as pd
 import pyproj
 
-'''
-def callTheTeam(team):
-    var28_df = var28_df.loc[var28_df['TEAMCODE'] == WhatTeamTypeIsNeeded]
-    return var28_df
-'''
-WhatTeamTypeIsNeeded = 'Weekends'
+
+#Enter the name of the teeam for which the interactive map needs to be generated from the list :- ['APT/HTL' 'GO' 'HazMatHiPi' 'HighRise' 'PlanCk' 'SCH/INS' 'SpecialOps','Weekends']
+What_Team_Type_Is_Needed = 'HazMatHiPi'
 #shapefile = gpd.read_file("zipcode/ZIPCODE.shp")
+
+# Read the shape file
 shapefile = gpd.read_file("shapefile_houston.shp")
 shapefile.to_crs(pyproj.CRS.from_epsg(4326), inplace=True)
 
-#print(shapefile)
-
-var28_df = pd.read_csv("violationDistributionByZipAndTeamCode.csv")
-var28_df.rename(columns={"ZIPCODE":"ZIPCODE","ZIP":"COUNT"},inplace=True)
-#var28_df = var28_df[ZIPCODE].astype('int64')
-#var28_df = var28_df[COUNT].astype('int64')
-#print(list(var28_df.columns))
-
-var28_df = var28_df.astype({'ZIPCODE': int})
-#var28_df= callTheTeam(WhatTeamTypeIsNeeded)
-var28_df = var28_df.loc[var28_df['TEAMCODE'] == WhatTeamTypeIsNeeded]
-#print(var28_df)
+#Read the csv which holds the count for the acses handled by each team grouped by zipcode
+ZIPCODE_Count_Data = pd.read_csv("violationDistributionByZipAndTeamCode.csv")
+#rename the columns
+ZIPCODE_Count_Data.rename(columns={"ZIPCODE":"ZIPCODE","ZIP":"COUNT"},inplace=True)
 
 
-#print(var28_df)
-var28_df.set_index("ZIPCODE")
+
+#Since the ZIPCODE in the read file is float we convert the type to int
+ZIPCODE_Count_Data = ZIPCODE_Count_Data.astype({'ZIPCODE': int})
+
+ZIPCODE_Count_Data = ZIPCODE_Count_Data.loc[ZIPCODE_Count_Data['TEAMCODE'] == What_Team_Type_Is_Needed]
+
+
+#Set ZIPCODE as the index
+ZIPCODE_Count_Data.set_index("ZIPCODE")
 
 import plotly.express as px
 import geopandas as gpd
 
-comb_shape = shapefile.merge(var28_df,on="ZIPCODE").set_index("ZIPCODE")
+comb_shape = shapefile.merge(ZIPCODE_Count_Data,on="ZIPCODE").set_index("ZIPCODE")
 
 
 
 
-titleText = "Number of Violations Reported by ZIP for "+WhatTeamTypeIsNeeded
+title_Text = "Number of Violations Reported by ZIP for "+What_Team_Type_Is_Needed
+
 fig = px.choropleth_mapbox(comb_shape,
                    geojson=comb_shape.geometry,
                    locations=comb_shape.index,
@@ -48,36 +47,15 @@ fig = px.choropleth_mapbox(comb_shape,
                     mapbox_style="open-street-map",
                    color_continuous_scale="Oranges",
                    zoom=9,
-                   width=1920,
-                   height=1080,
-                   title=titleText,
+                   #width=1920,
+                   width=1280,
+                   #height=1080,
+                   height=720,
+                   title=title_Text,
                    opacity=0.7
                    )
-fig.update_layout(
-    updatemenus=[
-        dict(
-            buttons=list([
-                dict(
-                    args=[WhatTeamTypeIsNeeded, "surface"],
-                    label="APT/HTLS",
-                    method="update"
-                ),
-                dict(
-                    args=[WhatTeamTypeIsNeeded, "heatmap"],
-                    label="GO",
-                    method="update"
-                )
-            ]),
-            direction="down",
-            pad={"r": 10, "t": 10},
-            showactive=True,
-            x=0.1,
-            xanchor="left",
-            y=1.1,
-            yanchor="top"
-        ),
-    ]
-)
+
+#Change the characteristics of the interactive pointer
 fig.update_layout(
     hoverlabel=dict(
         bgcolor="black",
@@ -86,5 +64,14 @@ fig.update_layout(
     )
 )
 
+
 fig.update_geos(fitbounds="locations", visible=False) 
+
+
+# Checks to handle / as this casues issues to the output path
+if What_Team_Type_Is_Needed.__contains__("/"):
+    WhatTeamTypeIsNeeded=What_Team_Type_Is_Needed.replace("/","-")
+    print(What_Team_Type_Is_Needed)
+fig.write_html(What_Team_Type_Is_Needed+".html")
+
 fig.show()
