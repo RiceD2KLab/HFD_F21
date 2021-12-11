492ad3844d9705745ad24d5035ba2e3f1ab3d6d2
Hannah Lei <hannah.lei129@gmail.com>
2021-12-11 16:07:42 -0600

    Updated with instructions and notes for how to run

--- wordcloud/wordCloudfromFreqGenerator.py
+++ wordcloud/wordCloudfromFreqGenerator.py
@@ -9,10 +9,17 @@ import matplotlib.pyplot as plt
 from PIL import Image
 from wordcloud.wordcloud import STOPWORDS
 
+# THIS FILE IS USED TO GENERATE A WORD CLOUD FROM THE FREQUENCY CSV FILE SAVED FROM wordFrequencyGenerator.py
+
+# Order of files:
+# 1. Run printViolationComment.py 
+# 2. Run wordFrequencyGenerator.py using the output from (1)
+# 3. Run this file to generate word cloud from the frequency csv file made from (2)
 
 reader = csv.reader(open('word_frequencies.csv', 'r',newline='\n'))
 d = defaultdict()
 
+# make dictionary with frequencies for each word
 for k,v in reader:
     v = v.strip()
     # print(k, v)
@@ -20,7 +27,7 @@ for k,v in reader:
         d[k] = float(v)
 
 
- # Generate a word cloud image
+# Generate a word cloud image
 
 stopwords = list(STOPWORDS)
 stopwords = stopwords + ["be", "all"]
@@ -29,7 +36,7 @@ for k, v in d.items():
     if k in stopwords:
         d[k] = 0
 
-# mask = np.array(Image.open('masks/D.jpeg'))
+# mask = np.array(Image.open('masks/D.jpeg')) # mask if you want the word cloud in a certain shape
 # wordcloud = WordCloud(width=4800, height=2400, collocations=False, mask=mask).generate_from_frequencies(d) 
 wordcloud = WordCloud(width=4800, height=2400, collocations=False).generate_from_frequencies(d) 
 
