ed2b5c1c307dce78c48d7e783d9c7b391fc13072
Joshua Washington <josh8551021@gmail.com>
2022-02-21 00:12:18 -0600

    Rename data_cleaning_functions module to data_wrangling

--- cleaning/data_wrangling.py
+++ cleaning/data_wrangling.py
@@ -1,8 +1,13 @@
+"""
+Data wrangling module 
+"""
 import os.path
 import re
 from enum import Enum
 import pandas as pd
 import glob
+
+from typing import List, Dict
 
 
 class FileType(Enum):
@@ -10,8 +15,8 @@ class FileType(Enum):
   CSV = 2
 
 
-def clean_text_data(raw_data: str, regex_replacements: dict[str, str],
-                    verbatim_replacements: dict[str, str]) -> str:
+def clean_text_data(raw_data: str, regex_replacements: Dict[str, str] = None,
+                    verbatim_replacements: Dict[str, str] = None) -> str:
   """
   Given a raw data string, cleans the data by using the regex and verbatim
   replacements.
@@ -20,6 +25,13 @@ def clean_text_data(raw_data: str, regex_replacements: dict[str, str],
   :param verbatim_replacements: Dictionary of verbatim replacements to make
   :return: Cleaned data
   """
+
+  if regex_replacements is None:
+    regex_replacements = {}
+
+  if verbatim_replacements is None:
+    verbatim_replacements = {}
+
   regexes = {re.compile(k): v for k, v in regex_replacements.items()}
   
   clean_text = raw_data
@@ -51,8 +63,8 @@ def clean_html(raw_html: str) -> str:
                          verbatim_replacements=removals_dict)
 
 
-def compile_datasets(datasets: list[pd.DataFrame],
-                     filter_cols: list[str]) -> pd.DataFrame:
+def compile_datasets(datasets: List[pd.DataFrame],
+                     filter_cols: List[str] = None) -> pd.DataFrame:
   """
   This method takes a number of datasets and a set of columns to delete from
   each data set if present, and combines all specified datasets into a single
@@ -61,7 +73,9 @@ def compile_datasets(datasets: list[pd.DataFrame],
   :param filter_cols: Columns to remove when filtering the data.
   :return: Compiled data from sources.
   """
-  
+  if filter_cols is None:
+    filter_cols = []
+
   for violation in datasets:
     for drop_col in filter_cols:
       if drop_col in violation:
@@ -71,6 +85,12 @@ def compile_datasets(datasets: list[pd.DataFrame],
 
 
 def output_to_excel(dataframe: pd.DataFrame, filename: str) -> None:
+  """
+  Converts a Pandas DataFrame into an Excel spreadsheet.
+  :param dataframe: DataFrame to be converted
+  :param filename: name of file to save
+  :return: None
+  """
   base, extension = os.path.splitext(filename)
   if extension != ".xlsx":
     filename = base + ".xlsx"
@@ -78,20 +98,26 @@ def output_to_excel(dataframe: pd.DataFrame, filename: str) -> None:
 
 
 def output_to_csv(dataframe: pd.DataFrame, filename: str) -> None:
+  """
+  Converts a Pandas DataFrame into a CSV file.
+  :param dataframe: DataFrame to be converted
+  :param filename: name of file to save
+  :return: None
+  """
   base, extension = os.path.splitext(filename)
   if extension != ".csv":
     filename = base + ".csv"
   
   dataframe.to_csv(filename, encoding="utf-8")
 
-def stack_datasets(folder_directory,extension):
+def stack_datasets(stack_dir, extension):
     """
     Takes a folder directory and extension for desired file type as input and 
     outputs a concatenated file of all of the files in the given folder. 
     """
     all_data = []
-    for file in glob.iglob(f'{directory}/*.{extension}'):
+    for file in glob.iglob(f'{stack_dir}/*.{extension}'):
         all_data.append(pd.read_csv(file))
 
     df = pd.concat(all_data, ignore_index=True)
-    df.to_csv(f'{directory}/ConcatenatedData.{extension}', index = False)
+    df.to_csv(f'{stack_dir}/ConcatenatedData.{extension}', index = False)
