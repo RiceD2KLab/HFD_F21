cc877cbe677b3b1ba1cadf87bc7847b52d544bde
annitachang <wc45@rice.edu>
2022-02-21 22:06:04 -0600

    add code used for generating placekey for add_violation data

--- cleaning/pk_infor+incident.py
+++ cleaning/pk_infor+incident.py
@@ -1,12 +1,19 @@
 from placekey.api import PlacekeyAPI
 import pandas as pd
 
-"""
-Takes as input the incident dataset, column name of the address column, output names and splits it into two datasets:
-1. Valid_incident_2018_2021.csv: rows that can be added a placekey.
-2. Invalid_incident_2018_2021.csv: rows that do not have a valid field in the column "Basic Incident Full Address".
-"""
-def filter_invalid(data, col_name, csv1, csv2):
+def split_address(data: str, col_name: str, valid_output_name: str, invalid_output_name: str):
+    """
+    Splits the input dataset based on whether the address column is complete. We assume the address
+    column to take the format [STREET] [CITY] [STATE] [ZIP] and is separated by space. If the address
+    is not of this format, we categorize the row as invalid. Exports the two dataframe to two .csv files.
+    Input:
+    data - a file path to the dataset
+    col_name - column name of the address column
+    valid_output_name - the name of the file with rows with valid addresses
+    invalid_output_name - the name of the file with rows with invalid addresses
+    Return:
+    None. Exports two dataframes as two csv files.
+    """
     incident = pd.read_csv(data)
     entries = []
     for i, row in incident.iterrows():
@@ -21,19 +28,21 @@ def filter_invalid(data, col_name, csv1, csv2):
             entries.append(i)
     #export each datasets
     df = incident.filter(items = entries, axis=0)
-    df.to_csv(csv1, index=False)
+    df.to_csv(invalid_output_name, index=False)
     incident.drop(entries, inplace=True)
-    incident.to_csv(csv2, index=False)
-# filter_invalid("../../datasets/Incident Data 2018_2021.csv", "Basic Incident Full Address", "Invalid_incident_2018_2021.csv", "Valid_incident_2018_2021.csv")
-# filter_invalid("../../datasets/Concatenated_Dataset_INFOR.csv", "Address", "Invalid_INFOR_2018_2021.csv", "Valid_INFOR_2018_2021.csv")
+    incident.to_csv(valid_output_name, index=False)
 
-"""
-Takes as input the valid incident dataset and adds placekey IDs for each row.
-If a placekey cannot be generated, 0 is added as a place filler.
-Exports the dataset with placekeys.
-"""
-def gen_placekey(valid, col_name):
-    incident = pd.read_csv(valid)
+def gen_placekey(data:str, col_name:str):
+    """
+    Tags each row with a placekey that serves as an unique identifier for each address.
+    If the Placekey API is unable to generate a valid placekey, we put 0 as a placeholder.
+    Input:
+    data - a file path to the dataset
+    col_name - column name of the address column
+    Return:
+    None. Directly modifies the input csv file.
+    """
+    incident = pd.read_csv(data, index_col=0)
     places = []
     for i, row in incident.iterrows():
         address = row[col_name]
@@ -42,11 +51,12 @@ def gen_placekey(valid, col_name):
         # we are assuming the street address has the format: [STREET] [CITY] [STATE] [ZIP]
         places.append({"query_id":str(i), "street_address": ' '.join(address[:-3]), "city": address[-3], "region": address[-2], 
     "postal_code": address[-1], "iso_country_code": "US"})
-    print("START LOOKUPS")
+    print("Start lookup")
     #performs lookup by batch
-    placekey_api_key = 'INSERT PLACEKEY API KEY HERE'
+    placekey_api_key = 'INSERT PLACEKEY API KEY HERE' #placekey is a free API and you can get an API key by registering on the website
     pk_api = PlacekeyAPI(placekey_api_key)
     placekeys = pk_api.lookup_placekeys(places)
+    ('finish lookup')
     placekey_col = []
     for pk_dict in placekeys:
         #if the address has a valid placekey, the dict would store placekey: [ID].
@@ -58,21 +68,44 @@ def gen_placekey(valid, col_name):
     #adds the new column into the dataframe
     incident['PlaceKey ID'] = placekey_col
     #exports
-    incident.to_csv(valid, index=False)
-# gen_placekey("Valid_incident_2018_2021.csv", "Basic Incident Full Address")
-# gen_placekey("Valid_INFOR_2018_2021.csv", "Address")
+    incident.to_csv(data, index=False)
 
-def filter_placekey(valid, csv1, csv2):
-    incident = pd.read_csv(valid)
+def split_placekey(data:str, valid_output_name:str, invalid_output_name:str):
+    """
+    Splits the input dataset based on whether the placekey column has a valid placekey.
+    Exports the two dataframe to two .csv files.
+    Input:
+    data - a file path to the dataset
+    valid_output_name - the name of the file with rows with valid placekey
+    invalid_output_name - the name of the file with rows with invalid placekey
+    Return:
+    None. Exports two dataframes as two csv files.
+    """
+    incident = pd.read_csv(data)
+    #df1:valid placekey
     df1 = incident[incident['PlaceKey ID'] != '0']
+    #df2:invalid placekey
     df2 = incident[incident['PlaceKey ID'] == '0']
-    df1.to_csv(csv1, index=False)
-    df2.to_csv(csv2, index=False)
-# filter_placekey("Valid_incident_2018_2021.csv", "Valid_incident_2018_2021_1.csv", "Valid_incident_2018_2021_2.csv")
-# filter_placekey("Valid_INFOR_2018_2021.csv", "Valid_INFOR_2018_2021.csv_1.csv", "Valid_INFOR_2018_2021.csv_2.csv")
+    #exports
+    df1.to_csv(valid_output_name, index=False)
+    df2.to_csv(invalid_output_name, index=False)
 
-
-
-
-
-
+def preprocess_add_violations(input_name:str, output_name:str):
+    """
+    Concatenates the address columns into one column 'STADDRESS' with space between each column entry.
+    Input:
+    input_name: a file path to the dataset
+    output_name: a string representing the name of the output file 
+    """
+    add_vio_data = pd.read_csv(input_name, index_col=0)
+    #zipcode column has type float, and has trailing .0, convert it to string
+    add_vio_data['ZIP'] = add_vio_data['ZIP'].astype(int).astype(str)
+    #replace empty fields with empty string
+    values = {"SUFFIX": "", "ZIP": "", "STATE": "", "CITY": ""} 
+    add_vio_data.fillna(value=values, inplace=True)
+    #concatenate in the order of street number, name, suffix
+    add_vio_data['STADDRESS'] = add_vio_data[['STNO', 'STNAME', 'SUFFIX', "CITY", "STATE", "ZIP"]].agg(' '.join, axis=1)
+    #drop the individual columns
+    add_vio_data.drop(['STNO', 'STNAME', 'SUFFIX', "CITY", "STATE", "ZIP"], axis=1, inplace=True)
+    #export
+    add_vio_data.to_csv(output_name, index=False)
