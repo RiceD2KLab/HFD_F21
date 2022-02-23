8c572661067123c550672dce5c3ad3c53db3600c
Joshua Washington <josh8551021@gmail.com>
2022-02-23 15:27:58 -0600

    Rename output files to be more specific

--- cleaning/hfd_data_placekey_tagging.py
+++ cleaning/hfd_data_placekey_tagging.py
@@ -19,14 +19,14 @@ def aggregate_address_fields(data: pd.DataFrame,
     address_col_name: the name of the new addresss column
   Return: the input DataFrame, with a concatenated address column
   """
-  
+
   # zipcode column has type float, and has trailing .0, convert it to string
   data['ZIP'] = data['ZIP'].astype(int).astype(str)
-  
+
   # replace empty fields with empty string
   values = {"SUFFIX": "", "ZIP": "", "STATE": "", "CITY": ""}
   data.fillna(value=values, inplace=True)
-  
+
   # concatenate in the order of street number, name, suffix
   data[address_col_name] = data[
     ['STNO', 'STNAME', 'SUFFIX', "CITY", "STATE", "ZIP"]].agg(' '.join, axis=1)
@@ -34,18 +34,21 @@ def aggregate_address_fields(data: pd.DataFrame,
             axis=1, inplace=True)
   return data
 
+
 if __name__ == "__main__":
   # aggregate STNO and STNAME columns in address and violation data to create
   # appropriate input for street_address argument placekey lookup function
   add_vio_data = pd.read_csv('Cleaned Address and Violation Data 2020_2021.csv',
                              index_col=0)
-  
+
   address_col = "STADDRESS"
   add_vio_data_with_placekey = gen_placekey_from_address(
     aggregate_address_fields(add_vio_data, address_col),
     address_col)
-  
+
   with_placekey, without_placekey = split_placekey(add_vio_data_with_placekey)
-  
-  with_placekey.to_csv("AV_with_PK.csv")
-  without_placekey.to_csv("AV_wo_PK.csv")
+
+  with_placekey.to_csv(
+    "Cleaned Address and Violation Data 2020_2021 Placekey.csv")
+  without_placekey.to_csv(
+    "Cleaned Address and Violation Data 2020_2021 No Placekey.csv")
