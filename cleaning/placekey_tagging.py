adb92d6750c4e0ec80e3b7f3288dcf58377f81ec
Joshua Washington <josh8551021@gmail.com>
2022-03-02 15:43:42 -0600

    Add placekey geolocation and validation

--- cleaning/placekey_tagging.py
+++ cleaning/placekey_tagging.py
@@ -1,12 +1,15 @@
 from typing import Tuple
 
 from placekey.api import PlacekeyAPI
+import placekey as pk
 import pandas as pd
+import cleaning.data_wrangling as wrangling
 
 # placekey is a free API and you can get an API key by registering on the website
 PLACEKEY_API_KEY = 'PLACEKEY_API_KEY_HERE'
 
 PLACEKEY_FIELD_NAME = 'PlaceKey ID'
+INVALID_PK = "0"
 
 
 def gen_placekey_from_address(data: pd.DataFrame,
@@ -44,7 +47,7 @@ def gen_placekey_from_address(data: pd.DataFrame,
   for pk_dict in placekeys:
     # if the address has a valid placekey, the dict would store placekey: [ID].
     # otherwise, the dict would store error: Invalid address.
-    key = pk_dict.get('placekey', "0")
+    key = pk_dict.get('placekey', INVALID_PK)
     # if there is a valid placekey, we append it
     # otherwise, we append a 0 as placeholder
     placekey_col.append(key)
@@ -60,18 +63,44 @@ def split_placekey(data: pd.DataFrame,
   Splits the input dataset based on whether the placekey column has a valid
   placekey.
   
-  This method requires that the
-  
   Input:
     data - a DataFrame representing the input dataset
     placekey_col_name - name of the placekey column in the dataset
   Return:
     A tuple of two DataFrames for valid and invalid data:
       pk_valid - a DataFrame containing rows with valid placekey
-      pk_invalid (Optional) - a DataFrame containing rows with invalid placekey
+      pk_invalid - a DataFrame containing rows with invalid placekey
   """
-  # df1:valid placekey
-  pk_valid = data[data[placekey_col_name] != '0']
-  # df2:invalid placekey
-  pk_invalid = data[data[placekey_col_name] == '0']
+  pk_valid = data[data[placekey_col_name] != INVALID_PK]
+  pk_invalid = data[data[placekey_col_name] == INVALID_PK]
   return pk_valid, pk_invalid
+
+
+def generate_geocode(data: pd.DataFrame,
+                     placekey_column: str = PLACEKEY_FIELD_NAME,
+                     latitude_column: str = "latitude",
+                     longitude_column: str = "longitude") -> pd.DataFrame:
+  """
+  Generate the latitude and longitude value of a location based on its placekey.
+  :param data: Dataset with a place key column to generate latitude and
+    longitude
+  :param placekey_column: column with placekey data
+  :param latitude_column: column to place latitude data
+  :param longitude_column: column to place longitude data
+  :return: A DataFrame with added latitude and longitude columns.
+  """
+  latitudes = []
+  longitudes = []
+  for placekey in data[placekey_column]:
+    try:
+      lat, lng = pk.placekey_to_geo(placekey)
+      latitudes.append(lat)
+      longitudes.append(lng)
+    except ValueError:
+      # If placekey not properly formatted, fill with inf
+      latitudes.append(float('inf'))
+      longitudes.append(float('inf'))
+
+  data[latitude_column] = latitudes
+  data[longitude_column] = longitudes
+  return data
