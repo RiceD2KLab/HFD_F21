0802825a5b72043f49dc76ec47977e37c76c957f
annitachang <wc45@rice.edu>
2022-03-07 15:42:43 -0600

    Updated code for feature engineering in inspection data

--- cleaning/feature_engineering.py
+++ cleaning/feature_engineering.py
@@ -55,3 +55,17 @@ full_data['Total_Violations'] = full_data['VIOLATIONCode'].apply(
    lambda x: sum(1 for list_item in eval(x) if type(list_item)!=float) if type(x)!=float else 0)
 
 full_data.to_csv('Full_Merged_Data_Binaries_Totals.csv')
+
+##Extract statistics for inspection data
+inspection = pd.read_csv("INFOR_2018_2021_pk.csv")
+inspec_num = len(inspection["Inspection #"].unique())
+# print(inspec_num)
+inspection.drop_duplicates(inplace=True)
+# print(len(inspection.index))
+cols = ["Inspection Type", "Application Type", "Result", "Section", "Team"]
+dfs = []
+for col in cols:
+    inspection[col] = inspection[col].str.strip().replace("", "N/A").fillna("N/A")
+    dfs.append(inspection[col].value_counts().append(pd.Series([inspec_num], index=[col])))
+df = pd.concat(dfs)
+df.to_csv("Inspection_features.csv", index=0)
