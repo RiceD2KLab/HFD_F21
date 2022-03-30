472d349f5ef0a7b03cf75712f460b37c60fa21a1
tessacannon <tec3@rice.edu>
2022-03-30 18:28:49 -0500

    Update HCADwrangling.py
    Changing wrangling to only include properties in Houston

--- cleaning/HCADwrangling.py
+++ cleaning/HCADwrangling.py
@@ -1,15 +1,15 @@
 import pandas as pd
 
 #import HCAD data - "buildings" (non-residential properties) and "all_properties"
-buildings = pd.read_csv('building_other.txt', delimiter="\t")
-all_properties = pd.read_csv('real_acct.txt', delimiter="\t")
-print(all_properties.head())
+buildings = pd.read_csv('building_other.txt', delimiter="\t",encoding = 'unicode_escape')
+all_properties = pd.read_csv('real_acct.txt', delimiter="\t",encoding = 'unicode_escape')
+houston_properties = all_properties[all_properties['mail_city']=='HOUSTON']
 unq_buildings = buildings.groupby('acct').agg(lambda x: list(x))
 
 #Inner join datasets on "acct" feature (unique identifier for each building) to eliminate residential properties
 
 # joined_data = unq_buildings.set_index('acct').join(all_properties.set_index('acct'))
-joined_data = pd.merge(unq_buildings, all_properties, on = 'acct', how = 'inner')
+joined_data = pd.merge(unq_buildings, houston_properties, on = 'acct', how = 'inner')
 # #Select relevant columns and export data
 # non_residential_properties = joined_data[['site_addr_1', 'site_addr_2','site_addr_3','impr_tp']]
 joined_data.to_csv('Non-Residential_Properties.csv')
