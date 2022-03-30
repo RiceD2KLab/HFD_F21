c78611a25892e01047ed7d1392212e0e38ad3e60
tessacannon <tec3@rice.edu>
2022-03-29 19:37:41 -0500

    Update keep_only_houston.py

--- cleaning/keep_only_houston.py
+++ cleaning/keep_only_houston.py
@@ -1,18 +1,16 @@
 from numpy import row_stack
 import pandas as pd
 
-data = pd.read_csv('Full_Merged_Data_TC.csv')
+data = pd.read_csv('Full_mergeData.csv')
 
 address_list = data['STADDRESS_x']
 
 for row in address_list:
     if type(row)!=float:
-        #index=int(data.loc[data['STADDRESS_x'] == row].index.values[0])
-        if type(row)!=float:
-            address_components = str.split(row)
-            if 'HOUSTON' not in address_components:
-                data.drop(data[data['STADDRESS_x'] == row].index, inplace=True)
+        address_components = str.split(row)
+        if 'HOUSTON' not in address_components:
+            data.drop(data[data['STADDRESS_x'] == row].index, inplace=True)
 
 
-data.to_csv('Full_Merged_Data_TC_Houston.csv')
+data.to_csv('Full_Merged_Data_Houston.csv')
 
