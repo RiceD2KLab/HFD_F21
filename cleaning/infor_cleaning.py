9ce78fad56818aeec7134a618fa31f9f792baa81
Joshua Washington <josh8551021@gmail.com>
2022-04-12 00:48:59 -0500

    Add main section to infor_cleaning

--- cleaning/infor_cleaning.py
+++ cleaning/infor_cleaning.py
@@ -1,30 +1,44 @@
-
 '''
 This file is used to aggregate the INFOR data that was has been provided over common placekeys.
 The file also drops columns that are unnecessary to our final model, and modifies some entries to common formats.
 
 The merged dataset is then outputted to be observed and viewed.
 '''
+import os.path
 
 import pandas as pd
 
-# Read the existing INFOR data with placekeys into a dataframe.
-df = pd.read_csv('INFOR_2018_2021_pk_2.csv')
+from cleaning.data_wrangling import output_to_csv
 
-# Drop unnecessary rows from the dataframe.
-df = df.drop(['Inspection #','#','A/P #','Rinspection Date','Scheduled','Completed','Assigned To','Assigned To (Name)','Assigned To Provider','Location','Schedule Order','Unnamed: 18'],axis=1)
+if __name__ == "__main__":
+  # Read the existing INFOR data with placekeys into a dataframe.
+  infor_dir = os.path.normpath("Data/INFOR Inspection Data")
+  df = pd.read_csv(
+    os.path.join(infor_dir, "Intermediate Datasets",
+                 "INFOR_2018_2021_pk_2.csv"), index_col=0)
 
-# Cleaning values for section
-df['Section'] = df['Section'].replace(to_replace = 'School', value ='SCH/INS') # Combine school codes into one
-df['Section'] = df['Section'].replace(to_replace = ' ', value ='GO') # Identify blank entries as general occupancy
-df['Section'] = df['Section'].fillna(value ='GO') # Identify blank entries as general occupancy
+  # Drop unnecessary rows from the dataframe.
+  df = df.drop(
+    ['Inspection #', '#', 'A/P #', 'Rinspection Date', 'Scheduled', 'Completed',
+     'Assigned To', 'Assigned To (Name)', 'Assigned To Provider', 'Location',
+     'Schedule Order', 'Unnamed: 18'], axis=1)
 
-# Aggregate the data by unique placekey
-grouped_df = df.groupby('PlaceKey ID').agg(lambda x: list(x))
+  # Cleaning values for section
+  df['Section'] = df['Section'].replace(to_replace='School',
+                                        value='SCH/INS')  # Combine school codes into one
+  df['Section'] = df['Section'].replace(to_replace=' ',
+                                        value='GO')  # Identify blank entries as general occupancy
+  df['Section'] = df['Section'].fillna(
+    value='GO')  # Identify blank entries as general occupancy
 
-# Take the first address, rather than a repeating list
-for x in range(len(grouped_df["Address"])):
+  # Aggregate the data by unique placekey
+  grouped_df: pd.DataFrame  = df.groupby('PlaceKey ID').agg(lambda x: list(x))
+
+  # Take the first address, rather than a repeating list
+  for x in range(len(grouped_df["Address"])):
     grouped_df["Address"][x] = grouped_df["Address"][x][0]
 
-# Export the aggregated dataframe
-grouped_df.to_csv('AggregatedINFOR.csv')
+  print(grouped_df.head())
+
+  # Export the aggregated dataframe
+  output_to_csv(grouped_df, "AggregatedINFOR.csv")
