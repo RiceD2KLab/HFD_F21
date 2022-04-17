5b4ee29f105013475a0c1c0fae99c4b49a09accb
Joshua Washington <josh8551021@gmail.com>
2022-04-17 17:15:02 -0500

    Create new data wrangling module

--- wrangling/__init__.py
+++ wrangling/__init__.py
@@ -1,5 +1,5 @@
 """
-Data wrangling module 
+Data wrangling module
 """
 import os.path
 import re
@@ -8,11 +8,6 @@ import pandas as pd
 import glob
 
 from typing import List, Dict
-
-
-class FileType(Enum):
-  EXCEL = 1
-  CSV = 2
 
 
 def clean_text_data(raw_data: str, regex_replacements: Dict[str, str] = None,
@@ -47,7 +42,7 @@ def clean_text_data(raw_data: str, regex_replacements: Dict[str, str] = None,
 
 def clean_html(raw_html: str) -> str:
   """
-  Given a string of HTML data, cleans the data using pre-defined HTML cleaning
+  Given a string of HTML data, cleans the data using pre-defined HTML wrangling
   replacements.
   :param raw_html: Raw HTML string to be cleaned
   :return: Cleaned HTML text
@@ -88,7 +83,7 @@ def stack_datasets(stack_dir: str, extension: str = "*") -> pd.DataFrame:
   """
   Takes a folder directory and extension for desired file type as input and
   outputs a DataFrame with the stacked datasets.
-  
+
   :param stack_dir: Directory with files to stack
   :param extension: file extension of files to be stacked
   :return: A DataFrame with the stacked datasets
@@ -107,14 +102,11 @@ def filter_rows(dataset: pd.DataFrame,
   :param unwanted_values: Columns and corresponding sets of values to ignore
   :return: A DataFrame with the unwanted rows filtered out.
   """
-  to_drop = []
-  for idx, row in dataset.iterrows():
-    for column, unwanted in unwanted_values.items():
-      if row[column] in unwanted:
-        to_drop.append(idx)
-        continue
+  for column, unwanted in unwanted_values.items():
+    for item in unwanted:
+      dataset = dataset[dataset[column] != item]
 
-  return dataset.drop(to_drop)
+  return dataset
 
 
 def filter_null(dataset: pd.DataFrame, col_names: List[str]) -> pd.DataFrame:
@@ -149,46 +141,3 @@ def trim_string_fields(dataset: pd.DataFrame) -> pd.DataFrame:
   """
   return dataset.applymap(lambda x: (x.strip() if isinstance(x, str) else x))
 
-
-def output_to_excel(dataframe: pd.DataFrame, filename: str,
-                    keep_index=True) -> None:
-  """
-  Converts a Pandas DataFrame into an Excel spreadsheet.
-  :param dataframe: DataFrame to be converted
-  :param filename: name of file to save, without extension
-  :return: None
-  """
-  base, extension = os.path.splitext(filename)
-  if extension != ".xlsx":
-    filename = base + ".xlsx"
-  dataframe.to_excel(filename, encoding="utf-8", index=keep_index)
-
-
-def output_to_csv(dataframe: pd.DataFrame, filename: str,
-                  keep_index=True) -> None:
-  """
-  Converts a Pandas DataFrame into a CSV file.
-  :param dataframe: DataFrame to be converted
-  :param filename: name of file to save, without an extension
-  :return: None
-  """
-  base, extension = os.path.splitext(filename)
-  if extension != ".csv":
-    filename = base + ".csv"
-
-  dataframe.to_csv(filename, encoding="utf-8", index=keep_index)
-
-
-def output_to_pkl(dataframe: pd.DataFrame, filename: str,
-                  keep_index=True) -> None:
-  """
-  Exports a Pandas DataFrame into a Pickle file.
-  Args:
-    dataframe: DataFrame to be converted
-    filename: name of the output file, without an extension
-  """
-  base, extension = os.path.splitext(filename)
-  if extension != ".pkl":
-    filename = base + ".pkl"
-
-  dataframe.to_pickle(filename, encoding="utf-8", index=keep_index)
